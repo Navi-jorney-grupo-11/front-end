135abfeb9dbaf8f908c01833433668207256622f
GPrince00 <gustavoprince0@gmail.com>
2021-09-26 19:11:48 -0300

    feat(hero): added a hero on home page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 import Header from '../components/Header';
 
@@ -6,20 +6,39 @@ export default function Home() {
   return (
     <Wrapper>
       <Header />
-      <h1>HH</h1>
+      <Hero>
+        <h1>HH um jeito tecnológico de fazer um <span>Happy Hour</span></h1>
+      </Hero>
     </Wrapper>
 
   )
 }
 
-
 const Wrapper = styled.div`
-    background-color: ${({ theme }) => theme.colors.primary};
-    height: 100vh;
-    width: 100vw;
-    user-select: none; /* supported by Chrome and Opera */
-    -webkit-user-select: none; /* Safari */
-    -khtml-user-select: none; /* Konqueror HTML */
-    -moz-user-select: none; /* Firefox */
-    -ms-user-select: none; /* Internet Explorer/Edge */
+  background-color: ${({ theme }) => theme.colors.primary};
+  height: 100vh;
+  width: 100vw;
+  user-select: none; /* supported by Chrome and Opera */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+`;
+
+const Hero = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  height: 80%;
+
+  h1 {
+    font-weight: 300;
+    font-size: 2.5rem;
+    width: 47rem;
+  }
+
+  span {
+    font-weight: 500;
+  }
 `;
